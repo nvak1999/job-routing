34ccf86a6a2126d36a149e557934fedb39d5c32d
Khuong <nvak1999001@gmail.com>
2023-01-01 10:39:06 +0700

    asd

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,34 +4,32 @@ import Container from "@mui/material/Container";
 import jobData from "../jobData.json";
 import JobCard from "../components/JobCard";
 import Pagination from "@mui/material/Pagination";
-
+import Box from "@mui/material/Box";
 function HomePage() {
   return (
-    <Container
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Grid
-        container
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-        }}
-      >
+    // <Container
+    // sx={{
+    //   display: "flex",
+    //   justifyContent: "space-between",
+    //   flexDirection: "column",
+    //   alignItems: "center",
+    // }}
+    // >
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
         {jobData.slice(0, 5).map((job) => (
-          <JobCard job={job} />
+          <Grid item xs={4}>
+            <JobCard job={job} />
+          </Grid>
         ))}
       </Grid>
-      <Pagination
-        sx={{ alignSelf: "center", m: 2 }}
-        count={4}
-        shape="rounded"
-      />
-    </Container>
+    </Box>
+    // {/* <Pagination
+    //   sx={{ alignSelf: "center", m: 2 }}
+    //   count={4}
+    //   shape="rounded"
+    // /> */}
+    // </Container>
   );
 }
 
